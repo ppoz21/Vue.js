fc5888dfe8a78cb3e6367045cf2d1aeef4362bd9
Paweł Palacz <ppalacz@edu.cdv.pl>
2020-03-17 15:46:47 +0100

    Added htmlcssprimer app code

---
+++ htmlcssprimer/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h4 class="bg-primary text-white text-center p-2">
+            Lista zadań użytkownika Adam
+        </h4>
+        <div class="container-fluid p-4">
+            <table class="table table-striped table-bordered table-sm">
+                <thead>
+                    <tr><th>Zadanie</th><th>Czy wykonano?</th></tr>
+                </thead>
+                <tbody>
+                    <tr v-for="t in tasks" v-bind:key="t.action">
+                        <td>{{t.action}}</td>
+                        <td>{{t.done}}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="form-group m-2">
+                <label>Nowy element:</label>
+                <input v-model="newItemTekst" class="form-control" />
+            </div>
+            <div class="text-center">
+                <button class="btn btn-primary" v-on:click="addNewTodo">
+                    Dodaj
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data: function () {
+            return {
+                tasks: [{action: "Kup kwiaty", done: false},
+                    {action: "Znajdź buty", done: false},
+                    {action: "Odbierz bilety", done: true},
+                    {action: "Zadzwoń do Janka", done: false}],
+                newItemText: "",
+            }
+        },
+        methods: {
+            addNewTodo() {
+                this.tasks.push({
+                    action: this.newItemText,
+                    done: false
+                });
+                this.newItemText = "";
+            }
+        }
+    }
+</script>
+
