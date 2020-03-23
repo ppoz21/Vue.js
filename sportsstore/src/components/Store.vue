875158a8199d767aededf1509e89797b924c56b9
Paweł Palacz <ppalacz@edu.cdv.pl>
2020-03-23 12:54:09 +0100

    Added sporststore app base code

---
+++ sportsstore/src/components/Store.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col bg-dark text-white">
+                <a class="navbar-brand">SKLEP SPORTOWY</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3 bg-info p-2">
+                <category-controls />
+            </div>
+            <div class="col-9 p-2">
+                <product-list />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ProductList from "./ProductList";
+    import CategoryControls from "./CategoryControls";
+    export default {
+        components: {CategoryControls, ProductList}
+    }
+</script>
